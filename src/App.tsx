ffcd5aa01faa21a0b1bf9be1948c10ee3b090b02
Ivan Soldo <ivan@calimero.network>
2023-09-24 18:02:59 +0200

    feat(app): add login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import LoginForm from './components/LoginForm/LoginForm';
 import RegisterForm from './components/RegisterForm/RegisterForm';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" Component={Home}/>
         <Route path="/register" Component={RegisterForm}/>
+        <Route path="/login" Component={LoginForm}/>
       </Routes>
     </BrowserRouter>
   );
