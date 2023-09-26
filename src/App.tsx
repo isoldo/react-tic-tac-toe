f9bf616263ac9b49b4ef76fd74fa16b7ed0d0cce
Ivan Soldo <ivan@calimero.network>
2023-09-26 12:38:25 +0200

    feat(app): add dashboard component and tab selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from 'react';
 import LoginForm from './components/LoginForm/LoginForm';
 import RegisterForm from './components/RegisterForm/RegisterForm';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { getCookie, removeCookie } from  "typescript-cookie";
+import { getCookie } from  "typescript-cookie";
 import GamesList from './components/Games/GamesList';
 import UsersList from './components/Users/UsersList';
-
-function Home() {
-  const [logout, setLogout] = useState(false);
-  const [navigateToLogin, setNavigateToLogin] = useState(false);
-
-  useEffect(() => {
-    if (logout) {
-      removeCookie("login");
-      setNavigateToLogin(true);
-    }
-  }, [logout])
-
-  return(
-    <>
-    { navigateToLogin && <Navigate to="/login"/>}
-    <div>
-      <button onClick={() => { setLogout(true) }}>Logout</button>
-    </div>
-    </>
-  );
-}
+import Dashboard from './components/Dashboard/Dashboard';
 
 function ProtectedRoute({ Component, ...rest }: any) {
   const cookie = getCookie("login");
@@ -43,7 +22,7 @@ function App() {
       <Routes>
         <Route path="/register" Component={RegisterForm}/>
         <Route path="/login" Component={LoginForm}/>
-        <Route path="/" element={<ProtectedRoute Component={Home}/>}/>
+        <Route path="/" element={<ProtectedRoute Component={Dashboard}/>}/>
         <Route path="/games" element={<ProtectedRoute Component={GamesList}/>}/>
         <Route path="/users" element={<ProtectedRoute Component={UsersList}/>}/>
       </Routes>
